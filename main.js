3ea91e1bbb01ccb6f8d5e55350390772ab9ed8d4
Jasmyn Hicks <jasmynh44@gmail.com>
2021-04-21 15:40:41 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var titleInput = document.querySelector("#title-input")
+var bodyInput = document.querySelector("#body-Input")
+var saveBtn = document.querySelector("#save-btn")
+var ideaSection = document.querySelector("#idea-section")
+
+saveBtn.addEventListener("click", handleSave)
+
+function handleSave() {
+  event.preventDefault()
+}
